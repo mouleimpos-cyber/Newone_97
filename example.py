305330978354808a409be3d5b3074d9ccbf2eeb0
mouleimpos-cyber <mouleimpos@gmail.com>
2025-11-10 11:05:31 +0530

    Add homepage and profile functions with user_id

---
+++ example.py
@@ -0,0 +1,10 @@
+user_id = "moule_97" #global variable
+
+def homepage():
+    print("Welcome to the homepage : ", user_id)
+
+def profile():
+    print("Welcome to the profile page : ", user_id)
+
+homepage()
+profile()
